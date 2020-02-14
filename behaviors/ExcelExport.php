58c90d5d0a79e2b98be6348e81457c68f61818a5
charles <charles.stolive@dayonmars.fr>
2020-02-14 19:48:39 +0100

    Modification des behaviors imports

--- behaviors/ExcelExport.php
+++ behaviors/ExcelExport.php
@@ -1,63 +1,65 @@
 <?php namespace Waka\ImportExport\Behaviors;
 
 use Backend\Classes\ControllerBehavior;
-use October\Rain\Exception\ApplicationException;
-use Waka\ImportExport\Models\ConfigExport;
-use Flash;
-use Lang;
-use Redirect;
-use Backend;
-use Session;
 use Excel;
+use Redirect;
+use Session;
+use Waka\ImportExport\Models\ConfigExport;
+
 class ExcelExport extends ControllerBehavior
 {
     protected $ExportPopupWidget;
 
-	public function __construct($controller)
+    public function __construct($controller)
     {
         parent::__construct($controller);
         $this->ExportPopupWidget = $this->createExportPopupWidget();
     }
-    
-    public function onExportPopupForm() {
+
+    public function onExportPopupForm()
+    {
         $model = post('model');
         Session::put('modelImportExportLog.targetModel', $model);
         $this->vars['ExportPopupWidget'] = $this->ExportPopupWidget;
         $this->vars['model'] = $model;
         return $this->makePartial('$/waka/importexport/behaviors/excelexport/_popup.htm');
     }
- 
-    public function onExportValidation() {
-        //$data = $this->ExportPopupWidget->getSaveData();
+
+    public function onExportValidation()
+    {
+        $data = $this->ExportPopupWidget->getSaveData();
         // $sessionKey = \Input::get('_session_key');
         // $iel = new \Waka\ImportExport\Models\ImportExportLog;
         // $iel->fill($data);
         // $iel->logeable_type = post('logeable_type');
-        // $iel->logeable_id = post('logeable_id');
+        $configExportId = $data['logeable_id'];
         // $file = $iel
         //     ->excel_file()
         //     ->withDeferred($sessionKey) // how to get this session key dynamically?
         //     ->first();
-        //le fichier est maintenant prêt à être traité. 
-        $configExportId = post('logeable_id');
-        return Redirect::to('backend/waka/crsm/contacts/makeexcel/'.$configExportId);
+        //le fichier est maintenant prêt à être traité.
+        //$configExportId = post('logeable_id');
+        return Redirect::to('backend/waka/crsm/contacts/makeexcel/' . $configExportId);
     }
-    public function makeTempexcel($id) {
+    public function makeTempexcel($id)
+    {
         $configExportId = 1;
         $configExport = ConfigExport::find($configExportId);
         Session::put('excel.configExportId', $configExportId);
         return Excel::download(new $configExport->type->class, 'test.xlsx');
-        
+
     }
-    public function makeexcel($id) {
+    public function makeexcel($id)
+    {
         $configExportId = $id;
         $configExport = ConfigExport::find($configExportId);
         Session::put('excel.configExportId', $configExportId);
         return Excel::download(new $configExport->type->class, 'test.xlsx');
-        
+
     }
 
-    public function createExportPopupWidget() {
+    public function createExportPopupWidget()
+    {
         $config = $this->makeConfig('$/waka/importexport/models/importexportlog/fields_popup_export.yaml');
         $config->alias = 'exportformWidget';
         $config->arrayName = 'export_array';
--- behaviors/ExcelImport.php
+++ behaviors/ExcelImport.php
@@ -1,39 +1,41 @@
 <?php namespace Waka\ImportExport\Behaviors;
 
 use Backend\Classes\ControllerBehavior;
-use October\Rain\Exception\ApplicationException;
-use Waka\ImportExport\Models\ConfigImport;
-use Flash;
-use Lang;
+use Excel;
 use Redirect;
 use Session;
-use Excel;
+use Waka\ImportExport\Models\ConfigImport;
+
 class ExcelImport extends ControllerBehavior
 {
     protected $ImportPopupWidget;
 
-	public function __construct($controller)
+    public function __construct($controller)
     {
         parent::__construct($controller);
         $this->ImportPopupWidget = $this->createImportPopupWidget();
     }
-    public function onImport() {
+    public function onImport()
+    {
         $configImportId = 1;
         $configImport = ConfigImport::find($configImportId);
         Session::put('excel.configImportId', $configImportId);
         Excel::import(new $configImport->type->class, plugins_path('waka/crsm/updates/excels/maj_contact.xlsx'));
         return Redirect::refresh();
     }
-    public function onImportPopupForm() {
+    public function onImportPopupForm()
+    {
         $model = post('model');
         Session::put('modelImportExportLog.targetModel', $model);
         $this->vars['ImportPopupWidget'] = $this->ImportPopupWidget;
         $this->vars['model'] = $model;
         return $this->makePartial('$/waka/importexport/behaviors/excelimport/_popup.htm');
     }
- 
-    public function onImportValidation() {
+
+    public function onImportValidation()
+    {
         $data = $this->ImportPopupWidget->getSaveData();
+        trace_log($this->ImportPopupWidget->getSaveData());
         $sessionKey = \Input::get('_session_key');
         $iel = new \Waka\ImportExport\Models\ImportExportLog;
         $iel->fill($data);
@@ -41,7 +43,7 @@ class ExcelImport extends ControllerBehavior
             ->excel_file()
             ->withDeferred($sessionKey) // how to get this session key dynamically?
             ->first();
-        //le fichier est maintenant prêt à être traité. 
+        //le fichier est maintenant prêt à être traité.
         //$iel->save();
         $configImportId = $data['logeable_id'];
         // trace_log(post('logeable_id'));
@@ -53,7 +55,8 @@ class ExcelImport extends ControllerBehavior
         return Redirect::refresh();
     }
 
-    public function createImportPopupWidget() {
+    public function createImportPopupWidget()
+    {
         $config = $this->makeConfig('$/waka/importexport/models/importexportlog/fields_popup_import.yaml');
         $config->alias = 'importformWidget';
         $config->arrayName = 'import_array';
