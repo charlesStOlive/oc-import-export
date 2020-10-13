a0d08e54902c2b828a1a64e047513efd0b8835c0
Charels <charles.stolive@gmail.com>
2020-10-13 12:28:09 +0200

    correction des noms de class update

--- updates/create_config_exports_table_u102.php
+++ updates/create_config_exports_table_u102.php
@@ -4,7 +4,7 @@ use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 use Schema;
 
-class CreateConfigExportsTable extends Migration
+class CreateConfigExportsTableU103 extends Migration
 {
     public function up()
     {
--- updates/create_config_imports_table_u103.php
+++ updates/create_config_imports_table_u103.php
@@ -4,7 +4,7 @@ use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 use Schema;
 
-class CreateConfigImportsTable extends Migration
+class CreateConfigImportsTableU103 extends Migration
 {
 
     public function up()
